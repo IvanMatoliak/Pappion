859d81e25a672107042ba856a8c5c2ea39bc9494
IvanMatoliak <matoliak.i@gmail.com>
2021-07-18 20:03:03 +0300

    Add User Entity

--- Pappion.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Pappion.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ Pappion.Domain/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pappion.Domain.Entities
+{
+    class User
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Avatar { get; set; }
+        public DateTime RegistrationTime { get; set; }
+
+        public byte Password { get; set; }
+        public byte Salt { get; set; }
+
+        public UserStatus Status { get; set; }
+
+        [NotMapped] public string FullName => $"{FirstName} {LastName}";
+    }
+    public enum UserStatus
+    {
+        Active,
+        Blocked,
+    }
+}
