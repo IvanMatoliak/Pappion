652e803b3c58b99ca8b8e0fec539780db7a4a14d
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2021-10-31 23:15:28 +0200

    PAPI-2: Add Blazor UI, add users page

---
+++ Pappion.Application/Admin/Users/FakeUserService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bogus;
+using Pappion.Domain.Entities;
+
+namespace Pappion.Application.Admin.Users
+{
+  public class FakeUserService : IUserService
+  {
+    public Task<User[]> GetAllAsync()
+    {
+      var fake = new Faker<User>()
+        .RuleFor(p => p.Id, f => Guid.NewGuid())
+        .RuleFor(p => p.Username, f => f.Internet.UserName())
+        .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+        .RuleFor(p => p.LastName, f => f.Name.LastName())
+        .RuleFor(p => p.Avatar, f => f.Internet.Avatar())
+        .RuleFor(p => p.Email, f => f.Internet.Email())
+        .RuleFor(p => p.PhoneNumber, f => f.Phone.PhoneNumber());
+
+      return Task.FromResult(fake.Generate(10).ToArray());
+    }
+  }
+}
---
+++ Pappion.Application/Admin/Users/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Pappion.Domain.Entities;
+
+namespace Pappion.Application.Admin.Users
+{
+  public interface IUserService
+  {
+    Task<User[]> GetAllAsync();
+  }
+}
